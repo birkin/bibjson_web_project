887e283bb54e545774b030d9e848cf264fea8eae
birkin <birkin.diana@gmail.com>
2017-11-09 15:21:04 -0500

    builds json response.

--- bib_ourl_api_app/views.py
+++ bib_ourl_api_app/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth import logout
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from bib_ourl_api_app.lib.openurl import from_openurl
 
 
 log = logging.getLogger(__name__)
@@ -15,28 +16,32 @@ log = logging.getLogger(__name__)
 def ourl_to_bib( request ):
     """ Converts openurl to bibjson. """
     log.debug( '\n\n\nstarting ourl_to_bib()...' )
-    # if v1_validator.validate_request( request.method, request.META.get('REMOTE_ADDR', ''), request.POST ) is False:
-    #     log.info( 'request invalid, returning 400' )
-    #     return HttpResponseBadRequest( '400 / Bad Request' )
-    # result_data = caller.request_exact( request.POST )
-    # interpreted_response_dct = caller.interpret_result( result_data )
-    # log.debug( 'returning response' )
-    # jsn = json.dumps( interpreted_response_dct, sort_keys=True, indent=2 )
-    # return HttpResponse( jsn, content_type='application/javascript; charset=utf-8' )
-    return HttpResponse( 'ourl_to_bib coming' )
+    start = datetime.datetime.now()
+    ourl = request.GET.get( 'ourl', None )
+    if not ourl:
+        return HttpResponseBadRequest( '400 / Bad Request -- no `ourl` openurl parameter')
+    log.debug( 'ourl, ```%s```' % ourl )
+    bib = from_openurl( ourl )
+    log.debug( 'type(bib), `%s`' % type(bib) )
+    log.debug( 'bib, ```%s```' % bib )
+    rtrn_dct = bib
+    rtrn_dct = {
+        'query': {
+            'ourl': ourl,
+            'date_time': str( start )
+        },
+        'response': {
+            'bib': bib,
+            'elapsed_time': str( datetime.datetime.now() - start )
+        }
+    }
+    jsn = json.dumps( rtrn_dct, sort_keys=True, indent=2 )
+    return HttpResponse( jsn, content_type='application/javascript; charset=utf-8' )
 
 
 def bib_to_ourl( request ):
     """ Converts bibjson to openurl. """
     log.debug( '\n\n\nstarting bib_to_ourl()...' )
-    if v2_request_validator.validate_bib_request( request.method, request.META.get('REMOTE_ADDR', ''), request.POST ) is False:
-        log.info( 'request invalid, returning 400' )
-        return HttpResponseBadRequest( '400 / Bad Request' )
-    ( patron_barcode, title, author, year ) = ( request.POST['patron_barcode'], request.POST['title'], request.POST['author'], request.POST['year'] )
-    result_dct = v2_request_bib_caller.request_bib( patron_barcode, title, author, year )
-    log.debug( 'returning response' )
-    jsn = json.dumps( result_dct, sort_keys=True, indent=2 )
-    return HttpResponse( jsn, content_type='application/javascript; charset=utf-8' )
     return HttpResponse( 'bib_to_ourl coming' )
 
 
